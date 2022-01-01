47fed60c15fb3e7fd74cc56dea97a252bef34def
Edwin Ho <noodleslove@icloud.com>
2021-12-31 19:36:15 -0800

    Add constants variables for Scraper

---
+++ app/constants.py
@@ -0,0 +1,17 @@
+default_subjects = [
+    "Hey handsome, you're looking extra fine today.",
+    "Want me to cook tonight? (Optional dress code: aprons only)",
+    "I hope you don't plan on wearing clothes when you get home.",
+    "I'm sorry you're having a bad day. I'll make sure you have a good night.",
+    "I'm going to hop in the shower... Care to join?",
+    "I think it's a Netflix and Chill kind of night. You down?",
+    "I always have a good night's sleep when I'm with you."
+]
+
+default_models = {
+    '三上悠亞': 'https://jable.tv/models/yua-mikami/',
+    '篠田優': 'https://jable.tv/models/shinoda-yuu/',
+    '楓可憐': 'https://jable.tv/models/kaede-karen/',
+    '相澤南': 'https://jable.tv/models/aizawa-minami/',
+    '桃乃木香奈': 'https://jable.tv/models/momonogi-kana/'
+}
