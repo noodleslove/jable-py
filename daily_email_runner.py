c595d39ab3b5ae5ef98312501156113e9e560131
Edwin Ho <noodleslove@icloud.com>
2022-01-07 23:16:36 -0800

    Add runner files

---
+++ daily_email_runner.py
@@ -0,0 +1,5 @@
+from app.scraper import Scraper
+
+if __name__ == '__main__':
+    s = Scraper()
+    s.send_daily_email()
---
+++ weekly_email_runner.py
@@ -0,0 +1,5 @@
+from app.scraper import Scraper
+
+if __name__ == '__main__':
+    s = Scraper()
+    s.format_weekly_email()
