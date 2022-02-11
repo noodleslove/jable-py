77f63ca9a958f8949386a52703abb9a579592391
Edwin Ho <noodleslove@icloud.com>
2022-02-11 14:56:03 -0800

    Add docstrings

--- app/automation_helpers.py
+++ app/automation_helpers.py
@@ -15,6 +15,19 @@ class Scheduler:
         hour: int = 0,
         dow: list[str] = None
     ) -> None:
+        """Add a new job with the given frequency to the Cron
+
+        Args:
+            command (str): The command to execute
+            comment (str): Specify the email address for removal later
+            minute (int, optional): At which minute the job will be executed.
+                Defaults to 0.
+            hour (int, optional): At which hour the job will be executed.
+                Defaults to 0.
+            dow (list[str], optional): At which day of week the job will be 
+                executed. `['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']`
+                Defaults to None.
+        """
         job = self.cron.new(command=command, comment=comment)
         job.minute.on(minute)
         job.hour.on(hour)
@@ -28,11 +41,21 @@ class Scheduler:
     # <-- End of add_job()
 
     def remove_job(self, comment: str):
+        """Remove all jobs for given email address
+
+        Args:
+            comment (str): The email address to remove
+        """
         self.cron.remove_all(comment=comment)
         self.cron.write()
     # <-- End of remove_job()
 
     def list_all_job(self):
+        """Return a list of all active jobs
+
+        Returns:
+            list: A list of all active jobs
+        """
         return [job for job in self.cron]
     # <-- End of list_all_job()
 
