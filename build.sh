1c9ef83022ace0f417b96ae62592d442989d3a94
Edwin Ho <noodleslove@icloud.com>
2022-08-02 00:44:54 -0700

    much better now

--- build.sh
+++ build.sh
@@ -3,9 +3,6 @@
 # get current directory
 currentDir=`pwd`
 
-# get all current jobs
-# currentJobs=`crontab -l`
-
 declare -a jobsToAdd
 
 jobsToAdd+=("0 0 * * * python3 $currentDir/fetch_runner.py")
@@ -13,7 +10,7 @@ jobsToAdd+=("0 0 * * * python3 $currentDir/daily_email_runner.py")
 jobsToAdd+=("0 0 * * 0 python3 $currentDir/weekly_email_runner.py")
 
 # install all dependencies
-# pip3 install -r requirements.txt
+pip3 install -r requirements.txt
 
 # setup automations
 (crontab -l 2> /dev/null; printf "%s\n" "${jobsToAdd[@]}") | sort -u | crontab
