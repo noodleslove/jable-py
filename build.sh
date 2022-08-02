2ed2cf1fb95b90893b605f0b063a1092fca8ea26
Edwin Ho <noodleslove@icloud.com>
2022-08-02 00:42:01 -0700

    setup script

---
+++ build.sh
@@ -0,0 +1,21 @@
+#! /usr/local/bin/bash
+
+# get current directory
+currentDir=`pwd`
+
+# get all current jobs
+# currentJobs=`crontab -l`
+
+declare -a jobsToAdd
+
+jobsToAdd+=("0 0 * * * python3 $currentDir/fetch_runner.py")
+jobsToAdd+=("0 0 * * * python3 $currentDir/daily_email_runner.py")
+jobsToAdd+=("0 0 * * 0 python3 $currentDir/weekly_email_runner.py")
+
+# install all dependencies
+# pip3 install -r requirements.txt
+
+# setup automations
+(crontab -l 2> /dev/null; printf "%s\n" "${jobsToAdd[@]}") | sort -u | crontab
+
+echo "All good!"
